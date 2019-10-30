990de8b7d550113aaf328a848721507f4d97f594
Camas <camas@hotmail.co.uk>
2019-10-30 03:53:25 +0000

    Fix rankmirror path in build_docker.sh

--- .travis/build_docker.sh
+++ .travis/build_docker.sh
@@ -6,7 +6,7 @@ set -e
 # Generate mirrorlist. Travis' servers are in the US
 curl -s "https://www.archlinux.org/mirrorlist/?country=US&protocol=https&use_mirror_status=on" \
     | sed -e 's/^#Server/Server/' -e '/^#/d' \
-    | ./rankmirrors -n 5 - > ./.travis/aur-ci-img/mirrorlist
+    | ./.travis/rankmirrors -n 5 - > ./.travis/aur-ci-img/mirrorlist
 
 # Build and upload image
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
