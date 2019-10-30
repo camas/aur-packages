6e58c3e754eb3e92255633392c195144c259e1bb
Camas <camas@hotmail.co.uk>
2019-10-30 04:06:26 +0000

    Fix docker login in non-interactive shell

--- .travis/build_docker.sh
+++ .travis/build_docker.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Show commands
+set -x
+
 # Fail immediately on error 
 set -e
 
@@ -9,7 +12,7 @@ curl -s "https://www.archlinux.org/mirrorlist/?country=US&protocol=https&use_mir
     | ./.travis/rankmirrors -n 5 - > ./.travis/aur-ci-img/mirrorlist
 
 # Build and upload image
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 docker build -t aur-ci ./.travis/aur-ci-img
 docker tag aur-ci "$DOCKER_USERNAME"/aur-ci
 docker push "$DOCKER_USERNAME"/aur-ci
