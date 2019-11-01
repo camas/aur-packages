b600a9aa84ec3cca8bb8d047dde84479242a1f3c
Camas <camas@hotmail.co.uk>
2019-11-01 17:07:52 +0000

    Download latest docker image build to use as cache

--- .travis/build_docker.sh
+++ .travis/build_docker.sh
@@ -11,9 +11,13 @@ curl -s "https://www.archlinux.org/mirrorlist/?country=US&protocol=https&use_mir
     | sed -e 's/^#Server/Server/' -e '/^#/d' \
     | ./.travis/rankmirrors -n 5 - > ./.travis/aur-ci-img/mirrorlist
 
-# Build and upload image
+# Login to docker
 docker login -u "camas" -p "$DOCKER_PASSWORD"
+# Pull latest version to make use of cached steps
+docker pull camas/aur-ci:latest
+# Build
 docker build -t aur-ci -f ./.travis/aur-ci-img/Dockerfile .
+# Tag and push
 docker tag aur-ci camas/aur-ci:latest
 docker tag aur-ci camas/aur-ci:"$TRAVIS_BUILD_ID"
 docker push camas/aur-ci:latest
