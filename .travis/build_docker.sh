ff24b28fa645405d110ad81fb368888281faf982
Camas <camas@hotmail.co.uk>
2019-11-01 19:35:53 +0000

    Move mirrorlist update to Dockerfile to make better use of cache

--- .travis/build_docker.sh
+++ .travis/build_docker.sh
@@ -6,11 +6,6 @@ set -x
 # Fail immediately on error 
 set -e
 
-# Generate mirrorlist. Travis' servers are in the US
-curl -s "https://www.archlinux.org/mirrorlist/?country=US&protocol=https&use_mirror_status=on" \
-    | sed -e 's/^#Server/Server/' -e '/^#/d' \
-    | ./.travis/rankmirrors -n 5 - > ./.travis/aur-ci-img/mirrorlist
-
 # Login to docker
 docker login -u "camas" -p "$DOCKER_PASSWORD"
 
