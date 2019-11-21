04079244bb781bfde6704df75464883e7e8bc6c9
Camas <camas@hotmail.co.uk>
2019-11-21 00:16:42 +0000

    Update test_package.sh

--- .travis/test_package.sh
+++ .travis/test_package.sh
@@ -8,7 +8,7 @@ set -e
 
 # Pull latest image to save on compile time
 docker pull camas/aur-ci:"$TRAVIS_BUILD_ID"
-docker tag camas/aur-ci camas/aur-packages
+docker tag camas/aur-ci:"$TRAVIS_BUILD_ID" camas/aur-packages
 
 # Run tests
 python packager.py test "$PACKAGE"
