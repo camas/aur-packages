221920a5ce42bd5569f20f5d5c1db4f8dbdf4024
Camas <camas@hotmail.co.uk>
2020-02-25 23:58:52 +0000

    Run tests through packager on travis

--- .travis/test_package.sh
+++ .travis/test_package.sh
@@ -6,9 +6,14 @@ set -x
 # Fail immediately on error 
 set -e
 
+# Build packager
+cargo build --release --manifest-path src/Cargo.toml
+cp src/target/release/packager packager
+cp src/target/release/package_tester image/package_tester
+
 # Pull latest image to save on compile time
 docker pull camas/aur-ci:"$TRAVIS_BUILD_ID"
 docker tag camas/aur-ci:"$TRAVIS_BUILD_ID" camas/aur-packages
 
 # Run tests
-docker run --tmpfs /tmp --rm camas/aur-packages package_tester "$PACKAGE"
+./packager -vvvv test "$PACKAGE"
